b39de230593e25b70a84656c36a7cf0598f2d929
Heera Rana <heera.rana2@skybettingandgaming.com>
2023-12-27 16:16:57 +0000

    got connection working

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 
 async function dbConnect() {
   try {
+		console.log(process.env.MONGODB_URI)
     await mongoose.connect(process.env.MONGODB_URI)
   } catch (error) {
     console.error('Error connecting to MongoDB:', error)
